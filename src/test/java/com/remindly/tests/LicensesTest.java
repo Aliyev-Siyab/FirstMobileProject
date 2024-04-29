3064442a708dd38f41a86384f96cb871f13f7348
Aliyev-Siyab <149569244+Aliyev-Siyab@users.noreply.github.com>
2024-04-29 13:38:24 +0200

    AddLicensesTest

---
+++ src/test/java/com/remindly/tests/LicensesTest.java
@@ -0,0 +1,20 @@
+package com.remindly.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LicensesTest extends TestBase{
+
+    @BeforeMethod
+    public void precondition(){
+        app.getMainScreen().confirm();
+    }
+
+    @Test
+    public void tapLicensesPositiveTest(){
+        app.getMainScreen().tapOnMoreOptions();
+        app.getMainScreen().tapOnLicenses();
+        Assert.assertTrue(app.getLicenses().isLicensesTextPresent().contains("NOTICES FOR LIBRARIES"));
+    }
+}
