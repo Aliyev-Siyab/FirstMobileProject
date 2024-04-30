1ca7d4588049a634ed6c62658ab0462a1942e3cd
Aliyev-Siyab <149569244+Aliyev-Siyab@users.noreply.github.com>
2024-04-30 14:19:07 +0200

    ClassWork

--- src/test/java/com/remindly/tests/ReminderTests.java
+++ src/test/java/com/remindly/tests/ReminderTests.java
@@ -18,4 +18,37 @@ public class ReminderTests extends TestBase {
         app.getReminder().saveReminder();
         Assert.assertTrue(app.getMainScreen().isReminderTitlePresent().contains("Holiday"));
     }
+
+    @Test
+    public void addReminderWithDatePositiveTest(){
+        app.getMainScreen().tapOnAddReminder();
+        app.getReminder().enterReminderTitle("Holiday");
+        app.getReminder().tapOnDateField();
+        app.getReminder().swipeToMonth("future",1,"MAY");
+        app.getReminder().selectDate(0);
+        app.getReminder().tapOnYear();
+        app.getReminder().swipeToYear("future","2025");
+        app.getReminder().tapOnOk();
+        app.getReminder().saveReminder();
+        Assert.assertTrue(app.getMainScreen().isReminderDatePresent().contains("1/5/2025"));
+    }
+
+
+    @Test
+    public void addReminderWithTimePositive(){
+        app.getMainScreen().tapOnAddReminder();
+        app.getReminder().enterReminderTitle("Holiday");
+        app.getReminder().tapOnTimeField();
+        app.getReminder().selectTime("am",269,926,531,1195);
+        app.getReminder().tapOnOk();
+        app.getReminder().saveReminder();
+        Assert.assertTrue(app.getMainScreen().isReminderDatePresent().contains("9:30"));
+
+    }
+
+    // Check Repeat(verify Repeat off)
+
+    // check Repetition Interval (verify Interval)
+
+    // check Type of repetition (verify "Week")
 }
